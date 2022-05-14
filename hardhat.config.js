f58579958e53875de7fcce33189ce860268848f0
shubham-kanodia <shubhamkanodia123@gmail.com>
2022-05-14 11:26:09 +0530

    Update compiler version to 0.8.1

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ const TEST_NETWORK = "TEST_NETWORK";
 const LOCAL_NETWORK = "LOCAL_NETWORK";
 
 // By default network is set to local, change it to TEST_NETWORK to make a switch
-const NETWORK = TEST_NETWORK;
+const NETWORK = LOCAL_NETWORK;
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
@@ -22,6 +22,6 @@ if (NETWORK == TEST_NETWORK) {
 }
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.1",
   networks: networks
 };
