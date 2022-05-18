b1a7d1790ea98c90e7407c20009ebcadbdca0a15
Shreya <shreya@Shreyas-MacBook-Air.local>
2022-05-18 23:53:43 +0200

    save point, need to work on svg stuff

--- contracts/DappCampNFT.sol
+++ contracts/DappCampNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.1;
+pragma solidity ^0.6.1;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -10,21 +10,39 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract DappCampNFT is ERC721Enumerable, Ownable {
     uint256 public MAX_MINTABLE_TOKENS = 5;
 
-    constructor() ERC721("DappCamp NFT", "DCAMP") Ownable() {}
+    constructor() ERC721("Shreya's DappCamp NFT", "DCAMP") Ownable() {}
 
     string[] private collection = [
-        ""
+        "mediumpurple",
+        "orchid",
+        "indigo",
+        "lavender",
+        "thistle"
     ];
 
     function random(string memory input) internal pure returns (uint256) {
-        
+       return keccak256(abi.encodePacked(input));
     }
 
     function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal pure returns (string memory) {
-
+        uint256 rand = random(string(abi.encodePacked(keyPrefix, toString(tokenId))));
+        string memory output = sourceArray[rand % sourceArray.length];
+        return output;
     }
 
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
+        string[3] memory parts;
+        // keccak256
+        parts[0] = '<svg width="150" height="150">';
+        parts[1] = '<circle cx="75" cy="75" r="50" fill="lavender" />';
+        parts[2] = '</svg>';
+
+        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2]));
+        
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Color:', toString(tokenId), '", "description": "Purple polka dots are on chain NFTs I made for Dapp Camp.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        output = string(abi.encodePacked('data:application/json;base64,', json));
+
+        return output;
 
     }
 
