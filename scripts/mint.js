d57926af7976eab9458521ab1bd833c00efcaf6c
Thomas Hepner <hepner.thomas@gmail.com>
2022-05-17 22:20:28 -0700

    My on-chain NFT based on the DATA Index logo with some additional text.

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,8 +1,8 @@
 async function main() {
   const DappCampNFTFactory = await ethers.getContractFactory("DappCampNFT");
-  const dappCampNFT = DappCampNFTFactory.attach("CONTRACT_ADDR");
+  const dappCampNFT = DappCampNFTFactory.attach("0xa0b55BF2B73b4AB1D1FF98F004C440Da9ad34a8b");
 
-  await dappCampNFT.claim(1);
+  await dappCampNFT.claim(2);
 }
 
 main()
