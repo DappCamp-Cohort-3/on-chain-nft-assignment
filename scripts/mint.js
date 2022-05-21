2f8ddd0de9ecb2632ac1ae4a6815ec6bf256ae48
J P <jp@Js-MacBook-Pro.attlocal.net>
2022-05-21 08:58:43 -0400

    modified config for rinkeby, passed test, and tested mint

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,6 +1,6 @@
 async function main() {
   const DappCampNFTFactory = await ethers.getContractFactory("DappCampNFT");
-  const dappCampNFT = DappCampNFTFactory.attach("CONTRACT_ADDR");
+  const dappCampNFT = DappCampNFTFactory.attach("0x4674115d2b26510be95A9b9371ec64f84976Ea57");
 
   await dappCampNFT.claim(1);
 }
