bb941def04c2f27b41f5fec52f49cf7152b1711d
Shreya <shreya@Shreyas-MacBook-Air.local>
2022-05-21 20:19:59 -0700

    all tests passing

--- contracts/DappCampNFT.sol
+++ contracts/DappCampNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.1;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -21,27 +21,27 @@ contract DappCampNFT is ERC721Enumerable, Ownable {
     ];
 
     function random(string memory input) internal pure returns (uint256) {
-       return keccak256(abi.encodePacked(input));
+       return uint256(keccak256(abi.encodePacked(input)));
     }
 
     function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal pure returns (string memory) {
-        uint256 rand = random(string(abi.encodePacked(keyPrefix, toString(tokenId))));
+        uint256 rand = random(string(abi.encodePacked(keyPrefix, Strings.toString(tokenId))));
         string memory output = sourceArray[rand % sourceArray.length];
         return output;
     }
 
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
-        string[3] memory parts;
+        string[5] memory parts;
         // keccak256
         parts[0] = '<svg width="150" height="150">';
         parts[1] = '<circle cx="75" cy="75" r="50" fill="';
-        parts[2] = pluck(tokenId);
+        parts[2] = pluck(tokenId, "temp", collection);
         parts[3] = '" />';
         parts[4] = '</svg>';
 
         string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4]));
         
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Color:', toString(tokenId), '", "description": "Purple polka dots are on chain NFTs I made for Dapp Camp.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Color:', Strings.toString(tokenId), '", "description": "Purple polka dots are on chain NFTs I made for Dapp Camp.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
--- scripts/mint.js
+++ scripts/mint.js
@@ -1,8 +1,8 @@
 async function main() {
   const DappCampNFTFactory = await ethers.getContractFactory("DappCampNFT");
-  const dappCampNFT = DappCampNFTFactory.attach("CONTRACT_ADDR");
+  const dappCampNFT = DappCampNFTFactory.attach("0x11Ba10c66fc6BE806C6606a78Ac7FF940211a2F8");
 
-  await dappCampNFT.claim(1);
+  await dappCampNFT.claim(2);
 }
 
 main()
