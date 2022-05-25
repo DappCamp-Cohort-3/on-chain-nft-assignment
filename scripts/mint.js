8d1c9ed6d023ffc29bd9d05f8c133c66c71b0fef
SaumoPal97 <saumopal1997@gmail.com>
2022-05-26 04:35:52 +0530

    feat: add solidity code

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,6 +1,8 @@
 async function main() {
   const DappCampNFTFactory = await ethers.getContractFactory("DappCampNFT");
-  const dappCampNFT = DappCampNFTFactory.attach("CONTRACT_ADDR");
+  const dappCampNFT = DappCampNFTFactory.attach(
+    "0x174Fb066324B91aF6A5657257564bF77961B938C"
+  );
 
   await dappCampNFT.claim(1);
 }
