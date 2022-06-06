349538d28271211909bd12aa61531c2375607dee
blackqueentech <deliandjam@gmail.com>
2022-06-06 14:27:51 -0700

    configurations to run

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,6 +1,6 @@
 async function main() {
   const DappCampNFTFactory = await ethers.getContractFactory("DappCampNFT");
-  const dappCampNFT = DappCampNFTFactory.attach("CONTRACT_ADDR");
+  const dappCampNFT = DappCampNFTFactory.attach("0x93c281fE37AFda1B6EB278C8c1Fc77Fa80FEB50F");
 
   await dappCampNFT.claim(1);
 }
